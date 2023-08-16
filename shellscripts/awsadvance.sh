c38f7dac6706c69bf5a9b584bbd74d341fedd0c0
VenkataBhaskar <111889155+VenkataBhaskarr@users.noreply.github.com>
2023-08-16 10:37:15 +0530

    Update awsadvance.sh

--- shellscripts/awsadvance.sh
+++ shellscripts/awsadvance.sh
@@ -4,42 +4,68 @@
 ## Version : 1
 #######################
 
-#### S3 ####
+#!/bin/bash
 
-# creating a bucket 
-aws s3 create-bucket --bucket testingviacli964
-# granting permissions for lambda function to be invoked by s3 objects
+## This id is used to construct further ARN's
+AWS_ACCOUNT_ID = $(aws sts get-caller-identity --query 'Account' --output text)
+BUCKER_NAME    = "bhaskar-ultimate-bucket-bro"
+FUNCTION_NAME  = "bhaskar-ultimate-function-bro"
+EMAIL_ADDRESS  = "venkatabhaskarpuppala@gmail.com"
+ROLE_NAME      = "s3-lambda-sns"
+
+# Creating an IAM role for this project
+
+role_response=$(aws iam create-role --role-name s3-lambda-sns --assume-role-policy-document '{
+  "Version": "2012-10-17",
+  "Statement": [{
+    "Action": "sts:AssumeRole",
+    "Effect": "Allow",
+    "Principal": {
+      "Service": [
+         "lambda.amazonaws.com",
+         "s3.amazonaws.com",
+         "sns.amazonaws.com"
+      ]
+    }
+  }]
+}')
+role_arn = $(echo "$role_response") | jq -r '.Role.Arn'
+aws iam attach-role-policy --role-name "$ROLE_NAME" --policy-arn arn:aws:iam::aws:policy/AWSLambda_FullAccess
+aws iam attach-role-policy --role-name "$ROLE_NAME" --policy-arn arn:aws:iam::aws:policy/AmazonSNSFullAccess
+## Creating S3 bucket
+
+s3_bucket = $(aws s3api create-bucket --bucket "$BUCKET_NAME")
+
+# creating a zip 
+
+zip -r s3-lambda-function.zip ./s3-lambda-function
+sleep(2)
+
+## Creating lambda function
+
+lambda_function = $(aws lambda create-function --function-name "$FUNCTION_NAME" --runtime python3.6 --zip-file "fileb://./s3-lambda-function.zip" --handler "s3-lambda-function/s3-lambda-function.lambda_handler" --role "$role_arn")
+aws lambda add-permission --function-name "$FUNCTION_NAME" --statement-id "s3-lambda-sns" --action "lambda:InvokeFunction" --principal s3.amazonaws.com --source-arn "arn:aws:s3:::$BUCKET_NAME"
+LambdaFunctionArn="arn:aws:lambda:us-east-1:$aws_account_id:function:lambda-function"
+aws s3api put-bucket-notification-configuration \
+  --bucket "$bucket_name" \
+  --notification-configuration '{
+    "LambdaFunctionConfigurations": [{
+        "LambdaFunctionArn": "'"$LambdaFunctionArn"'",
+        "Events": ["s3:ObjectCreated:*"]
+    }]
+}'
 
 
+## Creating SNS service
 
-#### END ###
+sns_service_arn  = &(aws sns create-topic --name my-sns-lambda --output json | jq -r '.TopicArn')
 
+# Add SNS publish permission to the Lambda Function
+aws sns subscribe \
+  --topic-arn "$sns_service_arn" \
+  --protocol email \
+  --notification-endpoint "$EMAIL_ADDRESS"
 
-#### IAM config ####
-
-#### END ####
-
-
-
-#### Lambda ####
-
-# creation of lambda function
-aws lambda create-function --function-name mytestingfunction964 --runtime python3.6 --handler lambdafunc.lambda_handler --zip-file ./Desktop/Miscellenous/lambda.zip --role
-aws lambda add-permission --function-name mytestingfunction964 --statement-id s3-invoke --action "lambda:InvokeFunction" --principal s3.amazonaws.com --source-arn arn:aws:s3:::testingviacli964
-aws lamda create-event-source-mapping --function-name mytestingfunction964 --event-source-arn arn:aws:sns:::mytestingtopic964
-
-#### END ####
-
-
-
-#### SNS ####
-
-# creation of sns topic 
-aws sns create-topic --name mytestingtopic964
-# subscribing the arn topic via email to certain endpoint
-aws sns subscribe --topic-arn arn:aws:sns:::mytestingtopic964 --protocol email  --notification-endpoint venkatabhaskarpuppala@gmail.com
-
-#### END ####
 
 
 
